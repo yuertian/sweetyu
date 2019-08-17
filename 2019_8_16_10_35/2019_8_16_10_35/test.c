dcfe118562ff817a1d1101bb51f2f2ea0b8d86ad
yuertian <779661647@qq.com>
2019-08-17 17:30:45 +0800

    2019.8.17

---
+++ 2019_8_16_10_35/2019_8_16_10_35/test.c
@@ -0,0 +1,67 @@
+//int Jump_floor(int n)
+//{
+//	int i = 0;
+//	if (i ==1)
+//	    return 1;
+//	if (i == 2)
+//		return 2;
+//	return Jump_floor(n - 1) + Jump_floor(n - 2);
+//}
+//
+
+
+
+
+////11:36
+//
+//
+//void Move(char pos1, char pos2)
+//{
+//	printf("%c->%c ", pos1, pos2);
+//}
+///*
+//n:代表盘子个数
+//pos1：
+//pos2:
+//pos3:
+//*/
+//void Hanota(int n, char pos1, char pos2, char pos3)
+//{
+//	
+//	//终止条件
+//	//递归调用
+//	if (n == 1)
+//	    Move(pos1,pos3);
+//	else
+//	{
+//		Hanota()
+//	}
+//
+//	//if (n == 2)
+//	//{
+//	//	Move(pos1, pos2);
+//	//	Move(pos1, pos3);
+//	//	Move(pos2, pos3);
+//	//}
+//	//if (n == 3)
+//	//{
+//	//	Move(pos1, pos3);
+//	//	Move(pos1, pos2);
+//	//	Move(pos3, pos2);
+//	//	Move(pos1, pos3);
+//	//	Move(pos1, pos1);
+//	//	Move(pos2, pos3);
+//	//}
+//		
+//
+//	
+//}
+//int main()
+//{
+//	Hanota(1, 'A', 'B', 'C');
+//	Hanota(2, 'A', 'B', 'C');
+//	Hanota(3, 'A', 'B', 'C');
+//	return 0;
+//}
+
+
