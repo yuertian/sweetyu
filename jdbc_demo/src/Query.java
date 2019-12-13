139560612a417c0effa8ec79d2ba6448c0557a78
yuertian <779661647@qq.com>
2019-12-13 21:47:54 +0800

    jdbc_数据库连接Connection，Statement对象，ResultSet对象

---
+++ jdbc_demo/src/Query.java
@@ -0,0 +1,41 @@
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Query {
+
+    public static void main(String[] args) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            try {
+                //创建数据库连接 Connection
+                connection = DBUtil.getConnection2();
+                //根据数据库连接创建操作命令对象 Statement
+                statement = connection.createStatement();
+                String sql = "select id,name,chinese,math,english from exam_result";
+                //操作命令执行sql语句，返回结果集resultSet
+                //resultSet类似 List<Map<String, Object>>
+                statement.executeQuery(sql);
+                resultSet = statement.executeQuery(sql);
+                while (resultSet.next()) {
+                    int id = resultSet.getInt("id");
+                    String name = resultSet.getString("name");
+                    BigDecimal chinese = resultSet.getBigDecimal("chinese");
+                    BigDecimal math = resultSet.getBigDecimal("math");
+                    BigDecimal english = resultSet.getBigDecimal("english");
+                    System.out.printf("id = %s, name = %s, chinese = %s, math = %s, english = %s",
+                                        id, name, chinese, math, english);
+                }
+
+            } finally {
+                //释放资源
+                DBUtil.close(connection, statement, resultSet);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
