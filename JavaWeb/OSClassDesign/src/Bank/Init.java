038648bb9c614c79f2386c56af92178dace7d1a3
yuertian <779661647@qq.com>
2020-01-05 23:43:41 +0800

    购物车优化

---
+++ JavaWeb/OSClassDesign/src/Bank/Init.java
@@ -0,0 +1,16 @@
+package Bank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Init {
+
+    public static int resourse = Main.reType;
+    public static int process = Main.proCount;
+
+
+    public static void init() {
+        List<List<Integer>> list = new ArrayList<>();
+
+    }
+}
