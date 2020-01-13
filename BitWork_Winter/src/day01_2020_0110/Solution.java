c18a1a9cc3ac66165266e2f11ed0b1d1e774afa0
yuertian <779661647@qq.com>
2020-01-13 23:35:31 +0800

    实现函数 ToLowerCase() ，该函数接收一个字符串参数str ，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串 。

--- BitWork_Winter/src/day01_2020_0110/Solution.java
+++ BitWork_Winter/src/day01_2020_0110/Solution.java
@@ -1,13 +1,19 @@
 package day01_2020_0110;
 
-public class Solution {
+class Solution1 {
     public String toLowerCase(String str) {
-        int len = str.length();
-        for (int i = 0; i < len; i++) {
-            if (str[i] >= 'A' && str[i] <= 'Z') {
-                str[i] = str[i] + 32;
+        if (str == null) {
+            return str;
+        }
+        char[] c = str.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            if (c[i] >= 'A' && c[i] <= 'Z') {
+                c[i]+= 32;
             }
         }
-        return str;
+        String str1 = new String(c);
+        return str1;
     }
 }
+
+
