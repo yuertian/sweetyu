ba1b9a525d2ceca22cf07d2bb1d035b4c09b70bb
yuertian <779661647@qq.com>
2020-01-23 23:55:53 +0800

    在键入字符name时，按键可能会被长按，而字符可能被输入 1 次或多次。检查键盘输入的字符 typed。其中一些字符可能被长按，那么就返回 True,反扑否则返回false。

---
+++ BitWork_Winter/src/day04_2020_0114/IsLongPressedName.java
@@ -0,0 +1,19 @@
+package day04_2020_0114;
+
+public class IsLongPressedName {
+    public boolean isLongPressedName(String name, String typed) {
+        char[] c1 = name.toCharArray();
+        char[] c2 = typed.toCharArray();
+        char[] newChar = new char[26];
+        for (int i = 0; i < c2.length; i++) {
+            newChar[c2[i]] += 1;
+        }
+        for (int i = 0; i < c1.length; i++) {
+            newChar[c1[i]] -= 1;
+            if (newChar[c1[i]] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
