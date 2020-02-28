68d356c573b92074d351179cbc49d4cd7900f6f3
yuertian <779661647@qq.com>
2020-02-28 23:05:43 +0800

    删除链表的倒数第K个节点

---
+++ javaSE_Practice/src/nowCoder/RemoveNthFromEnd.java
@@ -0,0 +1,27 @@
+package nowCoder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode temp = head;
+        Map<Integer,ListNode> m = new HashMap<Integer,ListNode>();
+        int index=1;
+        while(temp!=null){
+            m.put(index++,temp);
+            temp = temp.next;
+        }
+        int size = m.size();
+        if(size == 1)
+            return null;
+        if(n == 1){
+            m.get(size-1).next = null;
+        }else if(n == size){
+            head = head.next;
+        }else{
+            m.get(size-n).next = m.get(size-n+2);
+        }
+        return head;
+    }
+}
